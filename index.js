0b59855f0c1ebe63b3e7d64321d7ed4eafd90e92
rajaniamin <rajaniamin8@gmail.com>
2023-03-20 22:22:40 +0530

    done

--- index.js
+++ index.js
@@ -1,52 +1,8 @@
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
-});
 
 
 (function($){
     "use strict"
-    //nav-bar
-    $(window).scroll(function(){
-        if($(this).scrollTop() >200){
-            $('.navbar').fadeIn('slow').css('display','flex');
-        }
-        // else{
-        //     $('.navbar').fadeOut('slow').css('display','none');
-        // }
-    });
-    
-    // $(".navbar-nav a").on('click', function (event){
-    // if(this.hash!==""){
-    //     event.preventDefault();
-    //     $('html, body').animate({
-    //         scrollTop:$(this.hash).offset().top - 45
-    //     },1500,'easeInOutExpo');
-    
-    //     if($(this).parents('.navbar-nav').length){
-    //         $('.navbar-nav .active').removeClass('active');
-    //         $(this).closest('a').addClass('active');
-    //     }
-    // }
-    // });
-    
-    //skillbar
-    // $('.skill').waypoint(function (){
-    // $('.progress .progress-bar').each(function (){
-    //     $(this).css("width",$(this).attr("aria-valuenow")+'%');
-    // });
-    // },{offset:'80%'});
-    
-    
-    
-    //typed
+  //typed
     if($('.typed-text-output').length==1){
         var typed_strings= $('.typed-text').text();
         var typed= new Typed('.typed-text-output',{
